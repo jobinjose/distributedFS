1947a0dc1a66927c6240d7e53f5f09e3f2d59b1e
Jobin Jose <jobinjose@outlook.com>
2017-12-13 02:16:26 +0000

    multiple fileserver

--- directorystore.py
+++ directorystore.py
@@ -7,15 +7,19 @@ print(choice + "....")
 
 if (choice == 'write'):
     s = shelve.open('dir_db')
+    lockdb = shelve.open('lock_db')
     try:
         while (1):
             f_name = input("Filename? ")
             if (f_name == 'KILL'):
                 break
             f_path = input("Filepath? ")
-            s[f_name] = f_path
+            f_port = input("Fileport? ")
+            s[f_name] = (f_path,f_port)
+            lockdb[f_name] = "U"
     finally:
         s.close()
+        lockdb.close()
 
 elif (choice == 'display'):
     s = shelve.open('dir_db')
@@ -25,3 +29,11 @@ elif (choice == 'display'):
     finally:
         s.close()
     print(existing)
+
+elif (choice == 'displayall'):
+    lockdb = shelve.open('dir_db')
+    try:
+        for key in lockdb:
+            print(key, lockdb[key])
+    finally:
+        lockdb.close()
--- fileserver.py
+++ fileserver.py
@@ -20,14 +20,15 @@ class mainclass:
             f_dir = s[filename]
         finally:
             s.close()
-        fullpath = os.path.join(f_dir, filename)
+        fullpath = os.path.join(f_dir[0], filename)
         f = open(fullpath,"r")
         return f.read()
-
+        return fullpath
 class redirect:
     def GET(self, path):
         web.seeother('/' + path)
 
 if __name__ == "__main__":
+    port1 = int(sys.argv[1])
     app = FilServer(urls, globals())
-    app.run(port=8866)
+    app.run(port=port1)
